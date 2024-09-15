3420294283ccbc3ea8fd9561ef6b55148ffc25a4
vamsi0874 <vamsim0874>
2024-09-15 16:24:17 +0530

    fix

--- lib/models.ts
+++ lib/models.ts
@@ -1,104 +1,206 @@
-import mongoose, { Document, Model } from "mongoose";
+// import mongoose, { Document, Model } from "mongoose";
+
+// // TypeScript interface for User
+// interface IUser extends Document {
+//     name: string;
+//     email: string;
+//     emailVerified?: Date;
+//     image?: string;
+//     password?: string;
+//     accounts: IAccount[];
+//     role: UserRole;
+//     isTwoFactorEnabled:boolean;
+// }
+
+// enum UserRole {
+//     ADMIN = "ADMIN",
+//     USER = "USER"
+// }
+
+// // TypeScript interface for Account
+// interface IAccount extends Document {
+//     id: string;
+//     userId: string;
+//     type: string;
+//     provider: string;
+//     providerAccountId: string;
+//     refresh_token?: string;
+//     access_token?: string;
+//     expires_at?: number;
+//     token_type?: string;
+//     scope?: string;
+//     id_token?: string;
+//     session_state?: string;
+//     // role: UserRole;
+// }
+// interface IVerificationToken extends Document {
+//     email: string;
+//     token: string;
+//     expires: Date;
+//   }
+// interface PasswordResetToken extends Document {
+//     email: string;
+//     token: string;
+//     expires: Date;
+//   }
+  
+//   // Schema for the VerificationToken
+ 
+// // Account Schema
+// const accountSchema = new mongoose.Schema<IAccount>({
+//     id: { type: String, required: true },
+//     userId: { type: String, required: true },
+//     type: { type: String, required: true },
+//     provider: { type: String, required: true },
+//     providerAccountId: { type: String, required: true },
+//     refresh_token: String,
+//     access_token: String,
+//     expires_at: Number,
+//     token_type: String,
+//     scope: String,
+//     id_token: String,
+//     session_state: String,
+//     // role: { type: String, enum: Object.values(UserRole), required: true } // Role is required in account
+// });
+
+// // User Schema
+// const userSchema = new mongoose.Schema<IUser>({
+//     name: String,
+//     email: { type: String, required: true },
+//     emailVerified: { type: mongoose.Schema.Types.Date },
+//     image: String,
+//     password: String,
+//     role: { type: String, enum: Object.values(UserRole), default: UserRole.USER }, // Default role is 'USER'
+    
+//     accounts: [accountSchema],
+    
+// });
+
+// const verificationTokenSchema = new mongoose.Schema<IVerificationToken>({
+//     email: { type: String, required: true, unique: true },  // Unique email
+//     token: { type: String, required: true, unique: true },  // Unique token for the email
+//     expires: { type: Date, required: true },                // Expiration time for the token
+//   });
+
+//   const PasswordResetTokenSchema = new mongoose.Schema<PasswordResetToken>({
+//     email: { type: String, required: true,unique: true },  // Unique email
+//     token: { type: String, required: true, unique: true },  // Unique token for the email
+//     expires: { type: Date, required: true },                // Expiration time for the token
+//   });
+
+
+ 
+// export const PasswordResetToken: Model<PasswordResetToken> = 
+//     mongoose.models?.PasswordResetToken || mongoose.model<PasswordResetToken>("PasswordResetToken", PasswordResetTokenSchema);
+
+     
+// export const VerificationToken: Model<IVerificationToken> = 
+// mongoose.models?.VerificationToken || mongoose.model<IVerificationToken>("VerificationToken", verificationTokenSchema);
+
+
+// export const User: Model<IUser> = mongoose.models?.User || mongoose.model<IUser>('User', userSchema);
+
+// export const Account: Model<IAccount> = mongoose.models?.Account || mongoose.model<IAccount>('Account', accountSchema);
+
+"use server"
+import mongoose from 'mongoose'
+
 
 // TypeScript interface for User
-interface IUser extends Document {
-    name: string;
-    email: string;
-    emailVerified?: Date;
-    image?: string;
-    password?: string;
-    accounts: IAccount[];
-    role: UserRole;
-    isTwoFactorEnabled:boolean;
+interface IUser extends mongoose.Document {
+  name: string;
+  email: string;
+  emailVerified?: Date;
+  image?: string;
+  password?: string;
+  accounts: IAccount[];
+  role: UserRole;
+  isTwoFactorEnabled: boolean;
 }
 
 enum UserRole {
-    ADMIN = "ADMIN",
-    USER = "USER"
+  ADMIN = "ADMIN",
+  USER = "USER",
 }
 
 // TypeScript interface for Account
-interface IAccount extends Document {
-    id: string;
-    userId: string;
-    type: string;
-    provider: string;
-    providerAccountId: string;
-    refresh_token?: string;
-    access_token?: string;
-    expires_at?: number;
-    token_type?: string;
-    scope?: string;
-    id_token?: string;
-    session_state?: string;
-    // role: UserRole;
+interface IAccount extends mongoose.Document {
+  id: string;
+  userId: string;
+  type: string;
+  provider: string;
+  providerAccountId: string;
+  refresh_token?: string;
+  access_token?: string;
+  expires_at?: number;
+  token_type?: string;
+  scope?: string;
+  id_token?: string;
+  session_state?: string;
 }
-interface IVerificationToken extends Document {
-    email: string;
-    token: string;
-    expires: Date;
-  }
-interface PasswordResetToken extends Document {
-    email: string;
-    token: string;
-    expires: Date;
-  }
-  
-  // Schema for the VerificationToken
- 
+
+interface IVerificationToken extends mongoose.Document {
+  email: string;
+  token: string;
+  expires: Date;
+}
+
+interface PasswordResetToken extends mongoose.Document {
+  email: string;
+  token: string;
+  expires: Date;
+}
+
 // Account Schema
 const accountSchema = new mongoose.Schema<IAccount>({
-    id: { type: String, required: true },
-    userId: { type: String, required: true },
-    type: { type: String, required: true },
-    provider: { type: String, required: true },
-    providerAccountId: { type: String, required: true },
-    refresh_token: String,
-    access_token: String,
-    expires_at: Number,
-    token_type: String,
-    scope: String,
-    id_token: String,
-    session_state: String,
-    // role: { type: String, enum: Object.values(UserRole), required: true } // Role is required in account
+  id: { type: String, required: true },
+  userId: { type: String, required: true },
+  type: { type: String, required: true },
+  provider: { type: String, required: true },
+  providerAccountId: { type: String, required: true },
+  refresh_token: String,
+  access_token: String,
+  expires_at: Number,
+  token_type: String,
+  scope: String,
+  id_token: String,
+  session_state: String,
 });
 
 // User Schema
 const userSchema = new mongoose.Schema<IUser>({
-    name: String,
-    email: { type: String, required: true },
-    emailVerified: { type: mongoose.Schema.Types.Date },
-    image: String,
-    password: String,
-    role: { type: String, enum: Object.values(UserRole), default: UserRole.USER }, // Default role is 'USER'
-    
-    accounts: [accountSchema],
-    
+  name: String,
+  email: { type: String, required: true },
+  emailVerified: { type: mongoose.Schema.Types.Date },
+  image: String,
+  password: String,
+  role: { type: String, enum: Object.values(UserRole), default: UserRole.USER },
+  accounts: [accountSchema],
 });
 
+// VerificationToken Schema
 const verificationTokenSchema = new mongoose.Schema<IVerificationToken>({
-    email: { type: String, required: true, unique: true },  // Unique email
-    token: { type: String, required: true, unique: true },  // Unique token for the email
-    expires: { type: Date, required: true },                // Expiration time for the token
-  });
+  email: { type: String, required: true, unique: true },
+  token: { type: String, required: true, unique: true },
+  expires: { type: Date, required: true },
+});
 
-  const PasswordResetTokenSchema = new mongoose.Schema<PasswordResetToken>({
-    email: { type: String, required: true,unique: true },  // Unique email
-    token: { type: String, required: true, unique: true },  // Unique token for the email
-    expires: { type: Date, required: true },                // Expiration time for the token
-  });
+// PasswordResetToken Schema
+const passwordResetTokenSchema = new mongoose.Schema<PasswordResetToken>({
+  email: { type: String, required: true, unique: true },
+  token: { type: String, required: true, unique: true },
+  expires: { type: Date, required: true },
+});
 
+// Export models if running on the server
+export const PasswordResetToken =
+  mongoose?.models?.PasswordResetToken || mongoose?.model<PasswordResetToken>("PasswordResetToken", passwordResetTokenSchema);
 
- 
-export const PasswordResetToken: Model<PasswordResetToken> = 
-    mongoose.models?.PasswordResetToken || mongoose.model<PasswordResetToken>("PasswordResetToken", PasswordResetTokenSchema);
+export const VerificationToken =
+  mongoose?.models?.VerificationToken || mongoose?.model<IVerificationToken>("VerificationToken", verificationTokenSchema);
 
-     
-export const VerificationToken: Model<IVerificationToken> = 
-mongoose.models?.VerificationToken || mongoose.model<IVerificationToken>("VerificationToken", verificationTokenSchema);
+export const User =
+  mongoose?.models?.User || mongoose?.model<IUser>("User", userSchema);
 
-
-export const User: Model<IUser> = mongoose.models?.User || mongoose.model<IUser>('User', userSchema);
-
-export const Account: Model<IAccount> = mongoose.models?.Account || mongoose.model<IAccount>('Account', accountSchema);
-
+export const Account =
+  mongoose?.models?.Account || mongoose?.model<IAccount>("Account", accountSchema);
--- middleware.ts
+++ middleware.ts
@@ -1,76 +1,109 @@
-// import { NextResponse } from "next/server";
-// import { auth, BASE_PATH } from "@/auth";
+
+
+// import NextAuth from "next-auth"
+// import authConfig from "./auth.config"
+
+// import { 
+//   DEFAULT_LOGIN_REDIRECT,
+//   apiAuthPrefix,
+//   publicRoutes,
+//   authRoutes 
+// } from '@/routes'
+
+
+// const { auth } = NextAuth(authConfig)
+
+//  export default auth((req) =>{
+//   const { nextUrl } = req
+//   const isLoggedIn = !!req.auth
+//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+//   const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+//   const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+  
+//   if (isApiAuthRoute) {
+//     return
+//   }
+
+//   // If the user is logged in and trying to access the login page, redirect them
+//   if (isAuthRoute) {
+//     if (isLoggedIn) {
+//       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+//     }
+//     return
+//   }
+
+//   // If the user is not logged in and trying to access a non-public route, redirect to login
+  
+//   if (!isLoggedIn && !isPublicRoute) {
+
+//     let callbackUrl = nextUrl.pathname
+//     if(nextUrl.search){
+//       callbackUrl+= nextUrl.search
+//     }
+
+//     const encodedCallbackUrl = encodeURIComponent(callbackUrl)
+//     return Response.redirect(new URL(`/auth/login?${encodedCallbackUrl}`, nextUrl))
+//   }
+  
+
+//   return
+// })
 
 // export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// };
+//   matcher: [
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//   ],
+// }
 
-// export default auth((req) => {
-//   const reqUrl = new URL(req.url);
-//   if (!req.auth && reqUrl?.pathname !== "/") {
-//     return NextResponse.redirect(
-//       new URL(
-//         `${BASE_PATH}/signin?callbackUrl=${encodeURIComponent(
-//           reqUrl?.pathname
-//         )}`,
-//         req.url
-//       )
-//     );
-//   }
-// });
-
-
-import NextAuth from "next-auth"
-import authConfig from "./auth.config"
+import { NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
 
 import { 
   DEFAULT_LOGIN_REDIRECT,
   apiAuthPrefix,
   publicRoutes,
   authRoutes 
-} from '@/routes'
+} from '@/routes';
 
+const authMiddleware = async (req:any) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-const { auth } = NextAuth(authConfig)
-
- export default auth((req) =>{
-  const { nextUrl } = req
-  const isLoggedIn = !!req.auth
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
-  
+  // Allow access to API auth routes without restrictions
   if (isApiAuthRoute) {
-    return
+    return NextResponse.next();
   }
 
-  // If the user is logged in and trying to access the login page, redirect them
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
-    }
-    return
+  // Redirect logged-in users away from the login page
+  if (isAuthRoute && isLoggedIn) {
+    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
   }
 
-  // If the user is not logged in and trying to access a non-public route, redirect to login
-  
+  // Redirect unauthenticated users to login for protected routes
   if (!isLoggedIn && !isPublicRoute) {
-
-    let callbackUrl = nextUrl.pathname
-    if(nextUrl.search){
-      callbackUrl+= nextUrl.search
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
     }
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
 
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl)
-    return Response.redirect(new URL(`/auth/login?${encodedCallbackUrl}`, nextUrl))
+    return NextResponse.redirect(
+      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+    );
   }
-  
 
-  return
-})
+  // Allow access to public routes or authenticated routes
+  return NextResponse.next();
+};
+
+export default authMiddleware;
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
   ],
-}
+};
